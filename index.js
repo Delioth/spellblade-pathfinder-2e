eaca6429f1768006e7ac54d61c52ea204f9dcfcd
Delioth M <Delioth>
2020-07-16 00:44:36 -0500

    Rework Arcane Blade Fixes #3

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const opn = require('opn');
+
 app.use(express.static('.'));
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  opn('http://localhost:3000');
+});
